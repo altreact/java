33c3c4a02efa3304a9cd8f0236afd76ad109f4f4
Eduardo Dominguez <alt.react@gmail.com>
2017-07-25 02:35:58 -0400

    initial greedy project, minus maven and spring framework.

---
+++ src/com/altreact/greedy/Main.java
@@ -0,0 +1,12 @@
+package com.altreact.greedy;
+
+import static com.altreact.greedy.SimplerOutput.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        outputEmptyLine();
+        outputLine("greedy");
+        outputLine("mah boi!!");
+    }
+}
---
+++ src/com/altreact/greedy/SimplerOutput.java
@@ -0,0 +1,16 @@
+package com.altreact.greedy;
+
+public class SimplerOutput {
+
+    static void outputEmptyLine() {
+        java.lang.System.out.println();
+    }
+
+    static void outputLine(String string) {
+        java.lang.System.out.println(string);
+    }
+
+    public static void output(String string) {
+        java.lang.System.out.println(string);
+    }
+}
