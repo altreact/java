23fbbd17feb55b1426a32258c1caa778b0f70ab1
Eduardo Dominguez <alt.react@gmail.com>
2017-07-25 02:10:35 -0400

    Delete Constraints.java

--- src/mario/output/pyramid/Constraints.java
+++
@@ -1,9 +0,0 @@
-package com.altreact.mario.output.pyramid;
-
-public class Constraints {
-
-    public  int numberOfRowsHighestAcceptableValue = 23;
-    public  int numberOfRowsLowestAcceptableValue = 0;
-    public  int outputOptionsHighestAcceptableValue = 2;
-    public  int outputOptionsLowestAcceptableValue = 1;
-}
