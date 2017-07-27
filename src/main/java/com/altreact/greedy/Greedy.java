3c35e765607b42380b5e8a73350f841ca71476b6
altreact <alt.react@gmail.com>
2017-07-27 03:23:04 -0400

    added maven and spring framework. basic system output for testing.a

---
+++ src/main/java/com/altreact/greedy/Greedy.java
@@ -0,0 +1,14 @@
+package com.altreact.greedy;
+
+import static com.altreact.greedy.SimpleOutput.*;
+
+public class Greedy {
+
+    public static void main(String[] args) {
+
+        outputEmptyLine();
+        outputLine("Greedy");
+        outputLine("Mah Bois!");
+        outputEmptyLine();
+    }
+}
---
+++ src/main/java/com/altreact/greedy/SimpleOutput.java
@@ -0,0 +1,16 @@
+package com.altreact.greedy;
+
+class SimpleOutput {
+
+    static void output(String string) {
+        java.lang.System.out.print(string);
+    }
+
+    static void outputLine(String string) {
+        java.lang.System.out.println(string);
+    }
+
+    static void outputEmptyLine() {
+        java.lang.System.out.println();
+    }
+}
