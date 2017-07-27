43967fc5d415841b41c51ef5161ba378e9d52bbe
Eduardo Dominguez <alt.react@gmail.com>
2017-07-27 03:23:30 -0400

    added maven and spring framework. basic system output for testing. no beans.xml for now.

--- src/main/java/com/altreact/greedy/Greedy.java
+++ src/main/java/com/altreact/greedy/Greedy.java
@@ -5,6 +5,10 @@ import static com.altreact.greedy.SimpleOutput.*;
 public class Greedy {
 
     public static void main(String[] args) {
+        displayMainMessage();
+    }
+
+    private static void displayMainMessage() {
 
         outputEmptyLine();
         outputLine("Greedy");
