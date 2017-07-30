39f74ae9e95ffafc1d7ca037970caa5ea83a6330
Eduardo Dominguez <alt.react@gmail.com>
2017-07-29 20:39:45 -0400

    code for greedy is complete and clean. no dependency injection or tests yet.a

--- src/main/java/com/altreact/greedy/Greedy.java
+++ src/main/java/com/altreact/greedy/Greedy.java
@@ -5,65 +5,24 @@ import static com.altreact.greedy.output.SimplerOutput.*;
 
 public class Greedy {
 
+    private static int[] coinValues = {100, 50, 25, 10, 5, 1};
+    private static String[] coinNames = {"dollar", "half dollar", "quarter", "dime", "nickel", "penny"};
+    private static int[] coinCount = {0, 0, 0, 0, 0, 0};
     private static double amountChangeIsNeededFor;
     private static int amountLeftToMakeChangeFor;
-
-    final private static int dollar = 100;
-    final private static int halfDollar = 50;
-    final private static int quarter = 25;
-    final private static int dime = 10;
-    final private static int nickel = 5;
-    final private static int penny = 1;
-
-    private static int numberOfDollars = 0;
-    private static int numberOfHalfDollars = 0;
-    private static int numberOfQuarters = 0;
-    private static int numberOfDimes = 0;
-    private static int numberOfNickels = 0;
-    private static int numberOfPennies = 0;
     private static int totalNumberOfCoins = 0;
-
-    // use arrays to clean up the following functions //////////////////////////////////////////////////////////////////
-
-    private static void deductCoinsFromAmountChangeIsNeededFor() {
-
-        numberOfDollars = deductFromMoneyAmountBy(dollar);
-        numberOfHalfDollars = deductFromMoneyAmountBy(halfDollar);
-        numberOfQuarters = deductFromMoneyAmountBy(quarter);
-        numberOfDimes = deductFromMoneyAmountBy(dime);
-        numberOfNickels = deductFromMoneyAmountBy(nickel);
-        numberOfPennies = deductFromMoneyAmountBy(penny);
-        totalNumberOfCoins = getTotalNumberOfCoins();
-    }
-
-    private static void outputTotalNumberOfCoinsInChange() {
-
-        outputEmptyLine();
-        outputLine("there are:\n");
-
-        printCoinAmount("dollar", numberOfDollars);
-        printCoinAmount("half dollar", numberOfHalfDollars);
-        printCoinAmount("quarter", numberOfQuarters);
-        printCoinAmount("dime", numberOfDimes);
-        printCoinAmount("nickel", numberOfNickels);
-        printCoinAmount("penny", numberOfPennies);
-
-        System.out.printf("\n for a total of %d coins in %.2f.\n", totalNumberOfCoins, amountChangeIsNeededFor);
-        outputEmptyLine();
-    }
-
-    private static int getTotalNumberOfCoins() {
-
-        return numberOfDollars + numberOfHalfDollars +
-                numberOfQuarters + numberOfDimes +
-                numberOfNickels + numberOfPennies;
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public static void main(String[] args) {
 
         start();
+    }
+
+    private static void start() {
+
+        displayProgramNameAndDescription();
+        getUserImputedAmountChangeIsNeededFor();
+        deductCoinsFromAmountChangeIsNeededFor();
+        outputTotalNumberOfCoinsInChange();
     }
 
     private static void displayProgramNameAndDescription() {
@@ -77,14 +36,6 @@ public class Greedy {
         outputEmptyLine();
     }
 
-    private static void start() {
-
-        displayProgramNameAndDescription();
-        getUserImputedAmountChangeIsNeededFor();
-        deductCoinsFromAmountChangeIsNeededFor();
-        outputTotalNumberOfCoinsInChange();
-    }
-
     private static void getUserImputedAmountChangeIsNeededFor() {
 
         double userImputedAmountChangeIsNeededFor = Keyboard.getUserImputedValueToMakeChangeFor();
@@ -92,17 +43,58 @@ public class Greedy {
         amountLeftToMakeChangeFor = decimalToInt(amountChangeIsNeededFor);
     }
 
-    private static int deductFromMoneyAmountBy(int amountToDeduct) {
+    private static int decimalToInt(double decimal) {
+        return (int) (decimal * 100);
+    }
+
+    private static double roundDecimalDownToNearestHundredthsPlace(double decimal) {
+
+        return 0.01 * Math.floor(decimal * 100.0);
+    }
+
+    private static int deductFromAmountToMakeChangeFor(int amountToDeduct) {
 
         int numberOfCoins = 0;
 
         while (amountLeftToMakeChangeFor - amountToDeduct >= 0) {
-
             amountLeftToMakeChangeFor -= amountToDeduct;
             numberOfCoins++;
         }
 
         return numberOfCoins;
+    }
+
+    private static void deductCoinsFromAmountChangeIsNeededFor() {
+
+        for (int x = 0; x < coinCount.length; x++) {
+            coinCount[x] = deductFromAmountToMakeChangeFor(coinValues[x]);
+        }
+
+        totalNumberOfCoins = getTotalNumberOfCoins();
+    }
+
+    private static int getTotalNumberOfCoins() {
+
+        int totalCoinCount = 0;
+
+        for (int coinAmount : coinCount) {
+            totalCoinCount += coinAmount;
+        }
+
+        return totalCoinCount;
+    }
+
+    private static void outputTotalNumberOfCoinsInChange() {
+
+        outputEmptyLine();
+        outputLine("there are:\n");
+
+        for (int x = 0; x < coinCount.length; x++) {
+            printCoinAmount(coinNames[x], coinCount[x]);
+        }
+
+        System.out.printf("\n for a total of %d coins in %.2f.\n", totalNumberOfCoins, amountChangeIsNeededFor);
+        outputEmptyLine();
     }
 
     private static void printCoinAmount(String coinName, int totalNumberOfCoinInChange) {
@@ -125,14 +117,5 @@ public class Greedy {
         String coinMessage = "    %d " + coinName + ",\n";
 
         System.out.printf(coinMessage, totalNumberOfCoinInChange);
-    }
-
-    private static int decimalToInt(double decimal) {
-        return (int) (decimal * 100);
-    }
-
-    private static double roundDecimalDownToNearestHundredthsPlace(double decimal) {
-
-        return 0.01 * Math.floor(decimal * 100.0);
     }
 }
